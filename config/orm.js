018f14d2d78dfc780ed6ecf71b0c7258ff924eda
Reena Mahapatra <reenam2017@gmail.com>
2018-08-25 23:20:33 -0700

    update

--- config/orm.js
+++ config/orm.js
@@ -44,35 +44,35 @@ var orm = {
      },
 
     //updateOne()
-    // updateOne: function(burgerId, callback) {
-    //     var queryString = "UPDATE burgers SET ? WHERE ?";
-    //
-    //     connection.query(queryString, [{devoured: true}, {id: burgerId}], function(error, result){
-    //         if(error) throw error;
-    //         callback(result);
-    //     });
-    // }
+     updateOne: function(burgerId, callback) {
+         var queryString = "UPDATE burgers SET ? WHERE ?";
+    
+         connection.query(queryString, [{devoured: true}, {id: burgerId}], function(error, result){
+             if(error) throw error;
+             callback(result);
+         });
+     }
     // Function that updates a single table entry
-    updateOne: function(table, objColVals, condition, callback) {
-  // Construct the query string that updates a single entry in the target table
-      var queryString = "UPDATE " + table;
-      queryString += " SET ";
-      queryString += objToSql(objColVals);
-      queryString += " WHERE ";
-      queryString += condition;
+//     updateOne: function(table, objColVals, condition, callback) {
+//   // Construct the query string that updates a single entry in the target table
+//       var queryString = "UPDATE " + table;
+//       queryString += " SET ";
+//       queryString += objToSql(objColVals);
+//       queryString += " WHERE ";
+//       queryString += condition;
 
-      // console.log(queryString);
+//       // console.log(queryString);
 
-      // Perform the database query
-      connection.query(queryString, function(error, result) {
-        if (error) {
-          throw err;
-        }
+//       // Perform the database query
+//       connection.query(queryString, function(error, result) {
+//         if (error) {
+//           throw err;
+//         }
 
-        // Return results in callback
-        callback(result);
-      });
-    }
+//         // Return results in callback
+//         callback(result);
+//       });
+//     }
 };
 
 //Exporting the orm to be used by others
